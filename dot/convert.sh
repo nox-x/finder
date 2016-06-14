28774b1210e59f5f31eb171c7704bceffbbeaa55
Sören Wegener <wegener92@gmail.com>
2016-06-14 22:02:33 +0200

    Convert also svg.

--- dot/convert.sh
+++ dot/convert.sh
@@ -1,1 +1,2 @@
-for name in *; do dot -Tpng $name -o /srv/www.wegenerd.de/dot/$name.png; done
+for name in *; do dot -Tpng $name -o /srv/www.wegenerd.de/dot/png/$name.png; done
+for name in *; do dot -Tsvg $name -o /srv/www.wegenerd.de/dot/svg/$name.svg; done

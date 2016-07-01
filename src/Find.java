3111868586723b1d80769501c2f6ded7bfce16ab
christoph r <christoph.ruhl@student.uni-kassel.de>
2016-07-01 15:39:38 +0200

    excluded unvisitable nodes with internal return statement

--- src/Find.java
+++ src/Find.java
@@ -16,8 +16,6 @@ public class Find{
 	private static int noOfNodes;
 	private static boolean[] visited;
 
-	private static int[] refs;
-
 	public static void main(String[] args) {
 		// it's just brute-forcing a bit
 
@@ -36,7 +34,6 @@ public class Find{
 		noOfNodes = lines.size() - 1;
 
 		edges = new int[noOfNodes][MAX_REFS];
-		refs = new int[noOfNodes];
 
 		// get unidirectional paths
 		for (int i = 0; i < noOfNodes; i++) {
@@ -57,7 +54,6 @@ public class Find{
 				if(i == j) continue;
 				if(hasPath(i,j)){
 					edges[i][k] = j;
-					refs[i]++;
 					k++;
 				}
 			}
@@ -68,7 +64,6 @@ public class Find{
 		// count all cases
 
 		visited[startNode] = true;
-		lowerRefs(startNode);
 		bruteForce(startNode, 1);
 
 		// generate output
@@ -77,6 +72,17 @@ public class Find{
 	}
 
 	private static void bruteForce(int currentNode, int i) {
+
+		for (int node: edges[currentNode]){
+			if(node == -1) break;
+			if(visited[node]) continue;
+			int c = 0;
+			for(int neighbor : edges[node]){
+				if(neighbor == -1) break;
+				if(!visited[neighbor]) c++;
+			}
+			if(c == 0) return;
+		}
 
 		for (int node : edges[currentNode]) {
 			if (node == -1) return;
@@ -94,42 +100,12 @@ public class Find{
 			}
 
 			visited[node] = true;
-			refs[node] = 0;
-			boolean allVis = true;
-			for(int n : edges[node]){
-				if(n == -1) break;
-				if(n != node && refs[n] > 0) refs[n]--;
-				if(refs[n] <= 0 && !visited[n]) allVis = false;
-			}
-			if(!allVis){
-				visited[node] = false;
-				resetRefs(node);
-				continue;
-			}
 			bruteForce(node, i + 1);
-			resetRefs(node);
 			visited[node] = false;
 		}
 	}
 
-	private static boolean lowerRefs(int node){
-		refs[node] = 0;
-		boolean allVis = true;
-		for(int n : edges[node]){
-			if(n == -1) break;
-			if(n != node && refs[n] > 0) refs[n]--;
-			if(refs[n] <= 0 && !visited[n]) allVis = false;
-		}
-		return allVis;
-	}
 
-	private static void resetRefs(int node){
-		for(int n : edges[node]){
-			if(n == -1) break;
-			if(n != node) refs[n]++;
-			refs[node]++;
-		}
-	}
 
 	private static boolean hasPath(int i, int j) {
 		return inputEdges.get(i).contains(j) || inputEdges.get(j).contains(i);
--- src/Tester.java
+++ src/Tester.java
@@ -31,10 +31,10 @@ public class Tester {
 
         proof("graph52a.txt", 909, time);
         proof("graph52b.txt", 788, time);
-//        proof("graph52c.txt", 1819, time);
-//        proof("graph52d.txt", 2570, time);
-//        proof("graph52e.txt", 3047, time);
-//        proof("graph52f.txt", 2729, time);
+        proof("graph52c.txt", 1819, time);
+        proof("graph52d.txt", 2570, time);
+        proof("graph52e.txt", 3047, time);
+        proof("graph52f.txt", 2729, time);
 //        proof("graph59a.txt", 115, time);
     }
 
